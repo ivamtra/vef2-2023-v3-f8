784f8d0de9e1ab55adb7ce53c50797b18ed9ea15
ivamtra <97352752+ivamtra@users.noreply.github.com>
2023-03-10 19:40:35 +0000

    bras

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,18 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-const port = 3000;
+// const port = 3000;
+
+const {
+  PORT: port = 3000,
+  SESSION_SECRET: sessionSecret,
+  DATABASE_URL: connectionString,
+} = process.env;
+
+if (!connectionString || !sessionSecret) {
+  console.error('Vantar gögn í env');
+  process.exit(1);
+}
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
